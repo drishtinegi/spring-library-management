67e1b8e4777a4bdf66f516f75549e3e21965be79
drishtinegi <drishtinegi05@gmail.com>
2026-07-03 00:40:57 +0530

    Completed Exercise 2: Verified Spring Dependency Injection

--- src/main/java/LibraryManagementApplication.java
+++ src/main/java/LibraryManagementApplication.java
@@ -4,15 +4,15 @@ import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MainApplication {
+public class LibraryManagementApplication {
     public static void main(String[] args) {
-        // 1. Load the XML configuration file from the classpath resources folder
+        // Load the Spring IoC Container
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // 2. Fetch the fully configured managed bean instance out of the Spring container
+        // Retrieve the BookService. Because of DI, it already has the BookRepository inside it!
         BookService libraryService = context.getBean("bookService", BookService.class);
 
-        // 3. Test execution
+        // Test it
         libraryService.registerBook();
     }
 }
